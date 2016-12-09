424a9dbe896bceac7300703a6785f114908931b1
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-12-08 17:15:27 -1000

    hmm, I can't get rollup to work

--- rollem.config.js
+++ rollem.config.js
@@ -1,11 +1,41 @@
-import flow from "rollup-plugin-flow";
+import flow from 'rollup-plugin-flow';
+import jsx from 'rollup-plugin-jsx'
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
-const plugins = [flow()];
-
-export default [
-  {
-    entry: "src/popup.js",
-    dest: "extension/dist/popup.js",
-    plugins,
-  },
+const plugins = [
+  flow(),
+  jsx({factory: 'React.createElement'}),
+  commonjs({
+    include: [
+      'node_modules/react/**',
+      'node_modules/react-dom/**',
+    ],
+    namedExports: {
+      'node_modules/react/react.js': ['PropTypes'],
+    },
+  }),
+  nodeResolve({
+    module: false,
+    browser: true,
+  }),
+  // babel({
+  //   babelrc: false,
+  //   exclude: 'node_modules/**',
+  //   presets: [
+  //     ['es2015', {modules: false}],
+  //     'react',
+  //   ],
+  // }),
 ];
+
+export default [{
+  entry: 'src/popup.js',
+  dest: 'extension/dist/popup.js',
+  format: 'iife',
+  moduleName: 'soundControl',
+  moduleId: 'sound-control',
+  plugins,
+  sourceMap: true,
+}];
--- src/popup.js
+++ src/popup.js
@@ -1,24 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function refreshTabList(tabs) {
-  var list = document.querySelector("ul#audible-tabs");
-  document.body.removeChild(list);
-
-  list = document.createElement("ul");
-  list.setAttribute("id", "audible-tabs");
-
-  for (var tab of tabs) {
-    var item = document.createElement("li");
-    item.textContent = `ex.${tab.id} - ${tab.url}`;
-    item.onclick = function() {
-      chrome.tabs.update(tab.id, {active: true});
-    };
-
-    list.appendChild(item);
+class Popup extends React.Component {
+  render() {
+    return <p>This is something, indeed</p>;
   }
-
-  document.body.appendChild(list);
 }
 
-chrome.tabs.query({audible: true}, function (tabs) {
-  refreshTabList(tabs);
-});
+// function refreshTabList(tabs) {
+//   var list = document.querySelector("ul#audible-tabs");
+//   document.body.removeChild(list);
+//
+//   list = document.createElement("ul");
+//   list.setAttribute("id", "audible-tabs");
+//
+//   for (var tab of tabs) {
+//     var item = document.createElement("li");
+//     item.textContent = `ex.${tab.id} - ${tab.url}`;
+//     item.onclick = function() {
+//       chrome.tabs.update(tab.id, {active: true});
+//     };
+//
+//     list.appendChild(item);
+//   }
+//
+//   document.body.appendChild(list);
+// }
+//
+// chrome.tabs.query({audible: true}, function (tabs) {
+//   refreshTabList(tabs);
+// });
+
+ReactDOM.render(<Popup/>, document.getElementById('app'));
